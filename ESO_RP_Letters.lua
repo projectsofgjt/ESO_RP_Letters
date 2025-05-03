a670471823a06024ab620fce2eb7fce6f89c5baf
Greg Talotta <projectsofgjt@gmail.com>
2025-05-03 14:31:49 -0400

    Attempt to use lib for menu icon and pannel

--- ESO_RP_Letters.lua
+++ ESO_RP_Letters.lua
@@ -2,14 +2,43 @@ EsoRpLetters = {}
 EsoRpLetters.name = "ESO_RP_Letters"
 
 function EsoRpLetters.Initialize()
-    d("ESO RP Letters initialize.")
-    zo_callLater(function()
-        d("ESO RP Letters initialize.")
-    end, 10000) -- dela
+    -- Register the button in the Game Menu Bar
+    EsoRpLetters.CreateGameMenuButton()
 end
 
+-- This function creates a button in the Game Menu Bar
+function EsoRpLetters.CreateGameMenuButton()
+    local panelData = {
+        type = "submenu",
+        name = "RP Letters",
+        tooltip = "View your RP letters",
+        -- Here we link a function to be called when this submenu is selected
+        func = EsoRpLetters.ShowLetterPanel,
+        -- Add this to the Game Menu Bar
+        category = "system",
+    }
+
+    -- Create the button using LibAddonMenu
+    LibAddonMenu2:RegisterAddonPanel("ESO_RP_Letters_Panel", panelData)
+
+    -- Optionally, you can create a simple menu entry using `LibAddonMenu` as well.
+    -- This is just a placeholder for later when we need to load the letters
+end
+
+-- Function to display the RP letters panel (currently just a blank screen)
+function EsoRpLetters.ShowLetterPanel()
+    d("show letter pannel")
+    -- Create a blank screen to be replaced later with the letter list
+    local panel = WINDOW_MANAGER:CreateTopLevelWindow("EsoRpLetters_Panel")
+    panel:SetDimensions(800, 600)  -- Set size for the panel
+    panel:SetAnchor(CENTER, GuiRoot, CENTER, 0, 0)
+    panel:SetHidden(false)  -- Show the panel
+
+    -- You can customize this panel later to display the letter list
+end
+
+
 function EsoRpLetters.OnAddOnLoaded(event, addonName)
-    d("ESO RP Letters loaded." .. addonName)
     if addonName == EsoRpLetters.name then
         EsoRpLetters.Initialize()
         EVENT_MANAGER:UnregisterForEvent(EsoRpLetters.name, EVENT_ADD_ON_LOADED)
