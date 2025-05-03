7e19742d9e6631b4d40e9e1f2032c82b43004f81
Greg Talotta <projectsofgjt@gmail.com>
2025-05-03 16:51:05 -0400

    refactor for everything that is done

--- ESO_RP_Letters.lua
+++ ESO_RP_Letters.lua
@@ -6,19 +6,6 @@ local menuSceneName = "EsoRpLetters"  -- Define the scene name
 local menuScene -- We'll initialize this after the addon is loaded
 local LMM2  -- We'll initialize this after the addon is loaded
 
-
--- Function to display the RP letters panel (currently just a blank screen)
-function EsoRpLetters.ShowLetterPanel()
-    logger:Info("show letter panel")
-    -- Create a blank screen to be replaced later with the letter list
-    local panel = WINDOW_MANAGER:CreateTopLevelWindow("EsoRpLetters_Panel")
-    panel:SetDimensions(800, 600)  -- Set size for the panel
-    panel:SetAnchor(CENTER, GuiRoot, CENTER, 0, 0)
-    panel:SetHidden(false)  -- Show the panel
-
-    -- You can customize this panel later to display the letter list
-end
-
 -- This function creates a button in the Game Menu Bar
 function EsoRpLetters.CreateGameMenuButton()
     logger:Info("Creating game menu button")
@@ -53,32 +40,6 @@ function EsoRpLetters.CreateGameMenuButton()
     logger:Info("Game menu button created successfully")
 end
 
-function EsoRpLetters.CreateScene()
-    logger.Info("initialize scene" )
-    menuScene = ZO_Scene:New(menuSceneName, SCENE_MANAGER) 
-    
-    -- -- Mouse standard position and background
-    -- MYADDON_MAIN_SCENE:AddFragmentGroup(FRAGMENT_GROUP.MOUSE_DRIVEN_UI_WINDOW)
-    -- MYADDON_MAIN_SCENE:AddFragmentGroup(FRAGMENT_GROUP.FRAME_TARGET_STANDARD_RIGHT_PANEL)
-    
-    -- --  Background Right, it will set ZO_RightPanelFootPrint and its stuff.
-    -- MYADDON_MAIN_SCENE:AddFragment(RIGHT_BG_FRAGMENT)
-    
-    -- -- The title fragment
-    -- MYADDON_MAIN_SCENE:AddFragment(TITLE_FRAGMENT)
-    
-    -- -- Set Title
-    -- ZO_CreateStringId("SI_MYADDON_MAIN_MENU_TITLE", "My Addon Name")
-    -- MYADDON_MAIN_TITLE_FRAGMENT = ZO_SetTitleFragment:New(SI_MYADDON_MAIN_MENU_TITLE)
-    -- MYADDON_MAIN_SCENE:AddFragment(MYADDON_MAIN_TITLE_FRAGMENT)
-    
-    -- -- Add the XML to our scene
-    -- MYADDON_MAIN_WINDOW = ZO_FadeSceneFragment:New(MyUINameInXML)
-    -- MYADDON_MAIN_SCENE:AddFragment(MYADDON_MAIN_WINDOW)
-    
-    
-end
-
 function EsoRpLetters.Initialize()
     logger:Info("Initializing ESO RP Letters...")
 
@@ -86,7 +47,7 @@ function EsoRpLetters.Initialize()
     LMM2 = LibMainMenu2
     LMM2:Init()
     -- Set scene
-    EsoRpLetters.CreateScene()
+    menuScene = ZO_Scene:New(menuSceneName, SCENE_MANAGER) 
     ZO_CreateStringId("STRID_ESO_RP_LETTERS_DISPLAY", "Letter Note Book");
 
     -- Initialize addon components
