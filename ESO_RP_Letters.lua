a80828d1311bda08669707cecebdb3b65a67e98c
Greg Talotta <projectsofgjt@gmail.com>
2025-05-03 13:55:31 -0400

    change

--- ESO_RP_Letters.lua
+++ ESO_RP_Letters.lua
@@ -6,7 +6,7 @@ function EsoRpLetters.Initialize()
 end
 
 function EsoRpLetters.OnAddOnLoaded(event, addonName)
-    d("ESO RP Letters loaded.")
+    d("ESO RP Letters loaded." .. addonName)
     if addonName == EsoRpLetters.name then
         EsoRpLetters.Initialize()
         EVENT_MANAGER:UnregisterForEvent(EsoRpLetters.name, EVENT_ADD_ON_LOADED)
