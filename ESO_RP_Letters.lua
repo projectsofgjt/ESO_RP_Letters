0667e79a3d158110d7fb17c16826b98897f0a43a
Greg Talotta <projectsofgjt@gmail.com>
2025-05-03 21:10:37 -0400

    remove problem line

--- ESO_RP_Letters.lua
+++ ESO_RP_Letters.lua
@@ -84,7 +84,7 @@ function EsoRpLetters.InitPanel()
     -- Create the scroll list control as a child of ZO_RightPanelFootPrint (the default container in this fragment)
     container = ZO_RightPanelFootPrint
     list = WINDOW_MANAGER:CreateControlFromVirtual("EsoRpLettersList", container, "ZO_ScrollList")
-    list:SetAnchorFill()
+    -- list:SetAnchorFill()
 
     EsoRpLetters.scrollList = list  -- Save reference for later
 
